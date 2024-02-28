ecaca4b7e9501066afc6c588c65f6b5144a98afb
setuna20xx <144708028+setuna20xx@users.noreply.github.com>
2024-02-28 15:18:04 +0900

    Update code_sign.bat

--- code_sign.bat
+++ code_sign.bat
@@ -1,1 +1,1 @@
-signtool.exe sign /f test.pfx /p $cert_pwd /tr http://ts.ssl.com /td sha256 /fd SHA256 dist\main.exe
+C:\Program Files (x86)\Windows Kits\10\App Certification Kit\signtool.exe sign /f test.pfx /p $cert_pwd /tr http://ts.ssl.com /td sha256 /fd SHA256 dist\main.exe
